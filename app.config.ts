f68b7af9a8832dcf3e5dad785df8b1459800fd9f
ryoid <ryanjconceicao@gmail.com>
2024-03-23 15:58:25 +0800

    test

--- app.config.ts
+++ app.config.ts
@@ -3,16 +3,10 @@ import { FontaineTransform } from "fontaine"
 
 export default defineConfig({
   server: {
-    preset: "cloudflare-pages-static",
+    preset: "cloudflare-pages",
     rollupConfig: {
       external: ["__STATIC_CONTENT_MANIFEST", "node:async_hooks"],
     },
-    // prerender: {
-    //   crawlLinks: true,
-    // },
-    // routeRules: {
-    //   "/**": { isr: true },
-    // },
   },
   vite: {
     worker: {
